7f07cfd0236282c636c49c17d81a300c8129451f
saiteja <saitejakolapalli@gmail.com>
2023-12-27 19:07:47 +0530

    nested if else

--- src/Basics/learning.java
+++ src/Basics/learning.java
@@ -1,7 +1,58 @@
 package src.Basics;
 
+import java.util.Scanner;
+
 public class learning {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+        //Scanner scanner = new Scanner(System.in);
+
+        // System.out.println("Enter your number");
+
+        // int number = scanner.nextInt();
+
+          String round1candidateStatus ="fail";
+        if (round1candidateStatus=="pass") {
+
+            System.out.println("You have cleared round 1");
+
+            String round2candidateStatus ="pass";
+
+            if (round2candidateStatus =="pass"){
+
+
+                System.out.println("you have cleared round 2");
+
+                String round3candidateStatus ="pass";
+
+
+                if (round3candidateStatus=="pass"){
+
+
+                    System.out.println("you have passed round 3");
+
+
+                }
+                else {
+
+                    System.out.println("you have failed round 3");
+                }
+
+
+
+            }
+            else{
+
+                System.out.println("you have failed round 2");
+
+            }
+        }
+else {
+
+            System.out.println("you have failed round 1");
+        }
+
+
+
     }
-}
+    }
