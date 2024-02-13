7ac08ac48aab8b8cd5244114d0c7d74c0ba6faa8
saiteja <saitejakolapalli@gmail.com>
2024-02-13 20:21:11 +0530

    all

--- src/Basic_practice/count_even_odd.java
+++ src/Basic_practice/count_even_odd.java
@@ -4,33 +4,62 @@ public class count_even_odd {
     public static void main(String[] args) {
 
 
-        int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//        int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//
+//
+//        int even_count = 0;
+//
+//        int odd_count =0;
+//
+//
+//        for (int i=0; i<num.length; i++){
+//
+//
+//              if(num[i]%2==0)
+//              {
+//
+//              even_count++;
+//
+//            }
+//              else {
+//
+//                  odd_count++;
+//
+//              }
+//
+//
+//        }
+//
+//        System.out.println("the even count is "+even_count+" the odd count is " +odd_count);
 
+
+
+
+        int num = 123456789;
 
         int even_count = 0;
 
         int odd_count =0;
 
 
-        for (int i=0; i<num.length; i++){
+        while (num>0){
 
 
-              if(num[i]%2==0)
-              {
+            int rem = num%10;
 
-              even_count++;
+         if(rem%2==0){
 
-            }
-              else {
+             even_count++;
+         }
+         else {
 
-                  odd_count++;
+             odd_count++;
+         }
 
-              }
-
+         num =num/10;
 
         }
 
-        System.out.println("the even count is "+even_count+" the odd count is " +odd_count);
-
+        System.out.println("The even count is "+ even_count+" The odd count is "+odd_count);
     }
 }
